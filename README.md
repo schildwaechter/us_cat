cd24ff3ea8cac4de2578dd6588b12b797f4917b8
Carsten Thiel <cat@frejbow.net>
2014-10-05 13:54:38 +0200

    directly modify system's evdev.xml

---
+++ README.md
@@ -0,0 +1,16 @@
+# us_cat - xkb layout
+
+The `us_cat` Layout is a xkb keybord layout map intended for use
+with a us keyboard for typing primarily german but also providing
+diacritics for french and some slavic languages in latin script.
+
+## installation
+
+Use the provided python script `evdev_xml_entry.py` to directly
+modify your system's `evdev.xml`.
+
+## open questions
+
+- repurpose `<CAPS>`?
+- do `de`-based layout?
+
--- evdev_xml_entry.py
+++ evdev_xml_entry.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python
+"""python script to enter us_cat in evdev rules -
+makes it available in xfce keyboard selector"""
+
 from lxml import etree
-from sys import stdout
 
-doc = etree.parse('/usr/share/X11/xkb/rules/evdev.xml')
-if len(doc.xpath("./layoutList/layout/configItem/name[text()='us']/../../variantList/variant/configItem/name[text()='cat']")) == 0:
-    doc.xpath("./layoutList/layout/configItem/name[text()='us']/../../variantList")[0].append( etree.fromstring("<variant><configItem><name>cat</name><description>USA - CaT Custom Layout</description></configItem></variant>"))
+EVDEV_FILENAME = '/usr/share/X11/xkb/rules/evdev.xml'
 
-doc.write(stdout,pretty_print=True)
+EVDEV = etree.parse(EVDEV_FILENAME)
+if len(EVDEV.xpath("./layoutList/layout/configItem/name[text()='us']"+
+    "/../../variantList/variant/configItem/name[text()='cat']")) == 0:
+    EVDEV.xpath("./layoutList/layout/configItem/name[text()='us']"+
+            "/../../variantList")[0].append(
+            etree.fromstring(
+                "<variant><configItem><name>cat</name><description>"+
+                "USA - CaT Custom Layout</description></configItem></variant>"
+                )
+            )
 
+NEWEVDEV = etree.tostring(EVDEV, pretty_print=True)
+
+with open(EVDEV_FILENAME, "w") as evdev_xml:
+    evdev_xml.write(NEWEVDEV)
 
