e5f59f49d430d35714312691606b606d2c646283
Carsten Thiel <cat@frejbow.net>
2014-10-12 22:55:29 +0200

    switch to python3

--- evdev_xml_entry.py
+++ evdev_xml_entry.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """python script to enter us_cat in evdev rules -
 makes it available in xfce keyboard selector"""
 
@@ -20,5 +20,5 @@ if len(EVDEV.xpath("./layoutList/layout/configItem/name[text()='us']"+
 NEWEVDEV = etree.tostring(EVDEV, pretty_print=True)
 
 with open(EVDEV_FILENAME, "w") as evdev_xml:
-    evdev_xml.write(NEWEVDEV)
+    evdev_xml.write(NEWEVDEV.decode('utf-8'))
 
